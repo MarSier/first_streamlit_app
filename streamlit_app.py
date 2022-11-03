3f895a2741ce3ad131550f2b1b62a3947a766fb5
marcin.sieradzan <marcin.sieradzan@capgemini.com>
2022-11-03 08:33:55 +0100

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruity_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruity_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
